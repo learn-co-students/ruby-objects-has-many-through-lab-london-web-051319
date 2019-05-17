2137377ad2083e5c35c4a07b75fe751d7a463946
Scott <galejscott@gmail.com>
2019-05-17 17:33:24 +0100

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+    attr_accessor :name, :song, :genre
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(song, genre)
+        Song.new(song, self, genre)
+    end
+
+    def songs
+        Song.all.select { |song| song.artist == self }
+    end
+
+    def genres
+        songs.map(&:genre)
+    end
+
+end
